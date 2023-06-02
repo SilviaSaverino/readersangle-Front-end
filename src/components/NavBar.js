abc7f5b28bc59a8fa6f0d5a65c15530b27b86766
SilviaSaverino <>
2023-06-02 13:10:44 +0000

    add avatar component for profile image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
@@ -55,7 +56,7 @@ const NavBar = () => {
         activeClassName={styles.Active}
         to={`/profiles/${currentUser?.profile_id}`}
       >
-        <img src={currentUser?.profile_image} />
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
       </NavLink>
   {currentUser?.username}</>;
 
