30eee433effa80ad1dbe1c4afdf0857509cf57dc
SilviaSaverino <>
2023-06-05 16:47:11 +0000

    add card content and edit card layout

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,27 +1,90 @@
 import React from "react";
+import {
+  Card,
+  Media,
+  OverlayTrigger,
+  Row,
+  Col,
+  Tooltip,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import styles from "../../styles/Post.module.css";
 
 const Post = (props) => {
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    review_count,
+    likes_count,
+    like_id,
+    title,
+    author,
+    content,
+    genre_filter,
+    // followed_count,
+    // post_status,
+    image,
+    updated_at,
+    postPage,
+  } = props;
 
-    const {
-        id,
-        owner,
-        profile_id,
-        profile_image,
-        review_count,
-        likes_count,
-        like_id,
-        title,
-        author,
-        content,
-        genre_filter,
-        // followed_count,
-        // post_status,
-        image,
-        updated_at
-    } = props
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
-  return <div>Post</div>;
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align_item-center">
+            <span>{updated_at}</span>
+            {is_owner && postPage && "logic here"}
+          </div>
+        </Media>
+
+        <Card.Body>
+          <Row>
+            <Col>
+              {author && (
+                <Card.Title className="text-center">{author}</Card.Title>
+              )}
+              {genre_filter && (
+                <Card.Subtitle className="text-center">
+                  {genre_filter}
+                </Card.Subtitle>
+              )}
+            </Col>
+            <Col>
+              {title && (
+                <Card.Title className="text-center">{title}</Card.Title>
+              )}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Link to={`/posts/${id}`}>
+                <Card.Img src={image} alt={title} />
+              </Link>
+            </Col>
+            <Col>
+              {content && (
+                <Card.Text className="text-center">{content}</Card.Text>
+              )}
+            </Col>
+          </Row>
+
+          
+        </Card.Body>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Post;
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,7 +7,6 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
-import axios from "axios";
 import Post from "./Post";
 
 function PostPage() {
@@ -34,15 +33,12 @@ function PostPage() {
 
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
+      <Col className="py-2 p-0 p-lg-2">
         <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts={setPost} />
+        <Post {...post.results[0]} setPosts={setPost} postPage/>
         <Container className={appStyles.Content}>
           Reviews
         </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular posts
       </Col>
     </Row>
   );
