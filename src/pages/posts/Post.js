08c19f2b529a010680b514355bf3b8e7c3a35696
SilviaSaverino <>
2023-06-05 14:29:14 +0000

    destructure props from post results

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,7 +1,26 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
+
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        review_count,
+        likes_count,
+        like_id,
+        title,
+        author,
+        content,
+        genre_filter,
+        // followed_count,
+        // post_status,
+        image,
+        updated_at
+    } = props
+
   return <div>Post</div>;
 };
 
