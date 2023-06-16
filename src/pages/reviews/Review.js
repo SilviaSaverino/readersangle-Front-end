d35576bc001ce351f05dcc22fc7bf1c6ce816a00
SilviaSaverino <>
2023-06-16 14:26:52 +0000

    add functionality to let user like or unlike other user reviews, and set review like count accordingly

--- src/pages/reviews/Review.js
+++ src/pages/reviews/Review.js
@@ -1,5 +1,147 @@
+// import React, { useState } from "react";
+// import { Media, Tooltip, OverlayTrigger } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import Avatar from "../../components/Avatar";
+// import { MoreDropdown } from "../../components/MoreDropdown";
+// import styles from "../../styles/Review.module.css";
+// import { useCurrentUser } from "../../contexts/CurrentUserContext";
+// import { axiosRes } from "../../api/axiosDefaults";
+// import ReviewEditForm from "./ReviewEditForm";
+
+// const Review = (props) => {
+//   const {
+//     profile_id,
+//     profile_image,
+//     owner,
+//     updated_at,
+//     content,
+//     id,
+//     setPost,
+//     setReviews,
+//     reviewlikes_count,
+//     reviewlike_id,
+//   } = props;
+
+//   const [showEditForm, setShowEditForm] = useState(false);
+
+//   const currentUser = useCurrentUser();
+//   const is_owner = currentUser?.username === owner;
+
+//   const handleDelete = async () => {
+//     try {
+//       await axiosRes.delete(`/reviews/${id}/`);
+//       setPost((prevPost) => ({
+//         results: [
+//           {
+//             ...prevPost.results[0],
+//             reviews_count: prevPost.results[0].reviews_count - 1,
+//           },
+//         ],
+//       }));
+
+//       setReviews((prevReviews) => ({
+//         ...prevReviews,
+//         results: prevReviews.results.filter((review) => review.id !== id),
+//       }));
+//     } catch (err) {}
+//   };
+
+//   const handleReviewLike = async () => {
+//     try {
+//       const { data } = await axiosRes.post("/reviewlikes/", { review: id });
+//       setReviews((prevReviews) => ({
+//         ...prevReviews,
+//         results: prevReviews.results.map((review) => {
+//           return review.id === id
+//             ? { ...review, reviewlikes_count: review.likes_count + 1, reviewlike_id: data.id }
+//             : review;
+//         }),
+//       }));
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   const handleReviewUnlike = async () => {
+//     try {
+//       await axiosRes.delete(`/reviewlikes/${reviewlike_id}/`);
+//       setReviews((prevReviews) => ({
+//         ...prevReviews,
+//         results: prevReviews.results.map((review) => {
+//           return review.id === id
+//             ? { ...review, reviewlikes_count: review.reviewlikes_count - 1, reviewlike_id: null }
+//             : review;
+//         }),
+//       }));
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <hr />
+//       <Media>
+//         <Link to={`/profiles/${profile_id}`}>
+//           <Avatar src={profile_image} />
+//         </Link>
+//         <Media.Body className="align-self-center ml-2">
+//           <span className={styles.Owner}>{owner}</span>
+//           <span className={styles.Date}>{updated_at}</span>
+//           {showEditForm ? (
+//             <ReviewEditForm
+//               id={id}
+//               profile_id={profile_id}
+//               content={content}
+//               profileImage={profile_image}
+//               setReviews={setReviews}
+//               setShowEditForm={setShowEditForm}
+//             />
+//           ) : (
+//             <p>{content}</p>
+//           )}
+//         </Media.Body>
+//         {is_owner && (
+
+//           <OverlayTrigger
+//                     placement="top"
+//                     overlay={<Tooltip>You can't like your own post!</Tooltip>}
+//                   >
+//                     <i className="far fa-heart" />
+//                   </OverlayTrigger>
+//                 ) : reviewlike_id ? (
+//                   <span onClick={handleReviewUnlike}>
+//                     <i className={`fas fa-heart ${styles.Heart}`} />
+//                   </span>
+//                 ) : currentUser ? (
+//                   <span onClick={handleReviewLike}>
+//                     <i className={`far fa-heart ${styles.HeartOutline}`} />
+//                   </span>
+//                 ) : (
+//                   <OverlayTrigger
+//                     placement="top"
+//                     overlay={<Tooltip>Log in to like posts!</Tooltip>}
+//                   >
+//                     <i className="far fa-heart" />
+//                   </OverlayTrigger>
+//                 )}
+//                 {reviewlikes_count}
+//                 <Link to={`/reviews/${id}`}>
+//                   <i className="far fa-comments" />
+//                 </Link>
+//                 <MoreDropdown
+//             handleEdit={() => setShowEditForm(true)}
+//             handleDelete={handleDelete}
+//           />
+
+//       </Media>
+//     </div>
+//   );
+// };
+
+// export default Review;
 import React, { useState } from "react";
-import { Media } from "react-bootstrap";
+import { Media, Tooltip, OverlayTrigger } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
@@ -8,13 +150,23 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
 import ReviewEditForm from "./ReviewEditForm";
 
-
 const Review = (props) => {
-  const { profile_id, profile_image, owner, updated_at, content, id, setPost, setReviews } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setReviews,
+    reviewlikes_count,
+    reviewlike_id,
+  } = props;
 
-  const [showEditForm, setShowEditForm] = useState(false);  
+  const [showEditForm, setShowEditForm] = useState(false);
 
-  const currentUser = useCurrentUser() ;
+  const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
   const handleDelete = async () => {
@@ -36,6 +188,46 @@ const Review = (props) => {
     } catch (err) {}
   };
 
+  const handleReviewLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/reviewlikes/", { review: id });
+      setReviews((prevReviews) => ({
+        ...prevReviews,
+        results: prevReviews.results.map((review) => {
+          return review.id === id
+            ? {
+                ...review,
+                reviewlikes_count: review.reviewlikes_count + 1,
+                reviewlike_id: data.id,
+              }
+            : review;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleReviewUnlike = async () => {
+    try {
+      await axiosRes.delete(`/reviewlikes/${reviewlike_id}`);
+      setReviews((prevReviews) => ({
+        ...prevReviews,
+        results: prevReviews.results.map((review) => {
+          return review.id === id
+            ? {
+                ...review,
+                reviewlikes_count: review.reviewlikes_count - 1,
+                reviewlike_id: null,
+              }
+            : review;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <hr />
@@ -47,20 +239,56 @@ const Review = (props) => {
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
           {showEditForm ? (
-            <ReviewEditForm 
-                id={id}
-                profile_id={profile_id}
-                content={content}
-                profileImage={profile_image}
-                setReviews={setReviews}
-                setShowEditForm={setShowEditForm}
+            <ReviewEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setReviews={setReviews}
+              setShowEditForm={setShowEditForm}
             />
           ) : (
             <p>{content}</p>
           )}
         </Media.Body>
-        {is_owner && (
-            <MoreDropdown handleEdit={() => setShowEditForm(true)} handleDelete={handleDelete} />
+        {is_owner ? (
+          <>
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own review!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+            {reviewlikes_count}
+            <MoreDropdown
+              handleEdit={() => setShowEditForm(true)}
+              handleDelete={handleDelete}
+            />
+          </>
+        ) : reviewlike_id ? (
+          <>
+            <span onClick={handleReviewUnlike}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+            {reviewlikes_count}
+          </>
+        ) : currentUser ? (
+          <>
+            <span onClick={handleReviewLike}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+            {reviewlikes_count}
+          </>
+        ) : (
+          <>
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like reviews!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+            {reviewlikes_count}
+          </>
         )}
       </Media>
     </div>
