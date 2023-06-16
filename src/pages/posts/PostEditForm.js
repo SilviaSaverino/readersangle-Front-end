7cd35bdb17f0ac0dc7f010448298913b269f5e42
SilviaSaverino <>
2023-06-16 12:10:31 +0000

    add form to create a review; implement functionalities to increase reviews count and style review form

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -1,19 +1,3 @@
-// import React, { useEffect, useRef, useState } from "react";
-
-// import Form from "react-bootstrap/Form";
-// import Button from "react-bootstrap/Button";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import Container from "react-bootstrap/Container";
-
-// import Upload from "../../assets/upload.png";
-// import Asset from "../../components/Asset.js";
-// import styles from "../../styles/PostCreateEditForm.module.css";
-// import appStyles from "../../App.module.css";
-// import btnStyles from "../../styles/Button.module.css";
-// import { Alert, Image } from "react-bootstrap";
-// import { useHistory, useParams } from "react-router";
-// import { axiosReq } from "../../api/axiosDefaults";
 import React, { useEffect, useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -8,38 +8,51 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import ReviewCreateForm from "../reviews/ReviewCreateForm";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
 
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [reviews, setReviews] = useState({ results: [] });
+
   useEffect(() => {
     const handleMount = async () => {
-        try {
-            const [{data: post}] = await Promise.all([
-                axiosReq.get(`/posts/${id}`),
-                // axios.get(`/reviews/?post=${id}`),
-                axiosReq.get(`poststatus/?post=${id}&?profile=${id}`),
-                
-            ])
-            setPost({results: [post] })
-            console.log(post)
-        } catch(err){
-            console.log(err)
-        }
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          // axios.get(`/reviews/?post=${id}`),
+          axiosReq.get(`poststatus/?post=${id}&?profile=${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
     };
 
-    handleMount()
-  }, [id])
-
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2">
         <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts={setPost} postPage/>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-          Reviews
+          {currentUser ? (
+            <ReviewCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setReviews={setReviews}
+            />
+          ) : reviews.results.length ? (
+            "Comments/Reviews"
+          ) : null}
         </Container>
       </Col>
     </Row>
