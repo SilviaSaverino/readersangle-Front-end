c4a766c55154299c99ca69902c8f906ee471986a
SilviaSaverino <>
2023-06-26 13:55:36 +0000

    fix typo to correctly show dropdown menu to edit profile

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -14,7 +14,7 @@ const Gears = React.forwardRef(({ onClick }, ref) => (
   />
 ));
 
-export const MoreDropdown = ({handleEdit, handleDelete}) => {
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
   return (
     <Dropdown className="ml-auto" drop="left">
       <Dropdown.Toggle as={Gears} />
@@ -30,7 +30,11 @@ export const MoreDropdown = ({handleEdit, handleDelete}) => {
         >
           <i className="fas fa-edit" />
         </Dropdown.Item>
-        <Dropdown.Item className={styles.DropdownItem} onClick={handleDelete} aria-label="delete">
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleDelete}
+          aria-label="delete"
+        >
           <i className="fas fa-trash-alt" />
         </Dropdown.Item>
       </Dropdown.Menu>
@@ -38,33 +42,33 @@ export const MoreDropdown = ({handleEdit, handleDelete}) => {
   );
 };
 
-export function ProfileEditDropdown({ id }) {
-    const history = useHistory();
-    return (
-      <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
-        <Dropdown.Toggle as={ThreeDots} />
-        <Dropdown.Menu>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit`)}
-            aria-label="edit-profile"
-          >
-            <i className="fas fa-edit" /> edit profile
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit/username`)}
-            aria-label="edit-username"
-          >
-            <i className="far fa-id-card" />
-            change username
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit/password`)}
-            aria-label="edit-password"
-          >
-            <i className="fas fa-key" />
-            change password
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-  }
+export const ProfileEditDropdown = ({ id }) => {
+  const history = useHistory();
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+      <Dropdown.Toggle as={Gears} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+          aria-label="edit-profile"
+        >
+          <i className="fas fa-edit" /> edit profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="far fa-id-card" />
+          change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="fas fa-key" />
+          change password
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
